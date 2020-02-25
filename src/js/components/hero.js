76be9e7fa221d0ee04de598da55d721ef9994ab3
Tim Elliott <telliott22@gmail.com>
2020-02-25 21:15:33 +0000

    :construction: added props to component

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -5,20 +5,56 @@ export default class Hero extends EventEmitter {
     constructor(options) {
         super()
         // this.el = options.el
+        this.element = this.getElement();
+        this.data = this.initData();
+        this.props = this.initProps();
         this.render()
+    }
+
+    initData() {
+        return {
+            text: 'this is the TEXT!!!!'
+        }
+    }
+
+    initProps() {
+
+        let dataSet = this.element.dataset;
+
+        let props = [];
+
+        for (const x in dataSet) {
+            if (dataSet.hasOwnProperty(x)) {
+                const prop = dataSet[x];
+
+                if (x.indexOf('tim') === 0) {
+
+                    let formattedKey = x.replace('tim', '');
+
+                    props[formattedKey[0].toLowerCase() + formattedKey.substr(1)] = prop;
+
+                }
+
+            }
+        }
+
+        return props;
+    }
+
+    getElement() {
+        return document.querySelector('[role=hero]');
     }
 
     render() {
 
-        const element = document.querySelector('[role=hero]');
+        console.log('props', this.props);
 
-        element.innerHTML = this.markup();
-
+        this.element.innerHTML = this.markup();
     }
 
     markup() {
         return `
-          <h1>Hello, World!</h1>
+          <h1>${this.data.text}!</h1>
         `;
     }
 
